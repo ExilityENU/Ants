f917e6558c9bb8cb76d5f4927f3febb7d8c0ed52
ExilityENU <40533412@live.napier.ac.uk>
2024-12-13 03:09:50 +0000

    new push/ fixed ant respawn, no more patches

--- Sim.py
+++ Sim.py
@@ -36,22 +36,22 @@ class Simulation:
     def draw(self, screen, cell_size):
         screen.fill((255, 255, 255))
         pass
-        # diplays the terrain
+        # displays the terrain
         for (x, y), terrain in self.environment.terrain.items():
             color = (200, 255, 200) if terrain == "grass" else (0, 0, 255) if terrain == "water" else (150, 150, 150)
             pygame.draw.rect(screen, color, (y * cell_size, x * cell_size, cell_size, cell_size))
 
-        # diplays the resources
+        # displays the resources
         for resource in self.environment.resources:
             pygame.draw.circle(screen, (0, 255, 0), (
                 resource["pos"][1] * cell_size + cell_size // 2, resource["pos"][0] * cell_size + cell_size // 2), 5)
 
-        # diplays the nests
+        # displays the nests
         for nest in self.environment.nests:
             pygame.draw.circle(screen, (150, 75, 0),
                                (nest[1] * cell_size + cell_size // 2, nest[0] * cell_size + cell_size // 2), 15)
 
-        # diplays the agents
+        # displays the agents
         for agent in self.agents:
             color = (255, 2, 255) if isinstance(agent, WorkerAnt) else (255, 0, 0) if isinstance(agent,
                                                                                                  SoldierAnt) else (
